e32148a404143518332e7af36ff49d3e44df2fa7
yeongjinoh <oyj9109@snu.ac.kr>
2016-03-26 02:01:33 +0900

    fibonacci - Add big_integer structure to consider up to too big output

--- fibonacci/fibonacci.c
+++ fibonacci/fibonacci.c
@@ -1,16 +1,46 @@
 #include <stdio.h>
 
+#define INTEGER_UNIT 1000000
+#define BIG_INTEGER_LENGTH 10
+
+struct big_integer {
+	int unit_integer[BIG_INTEGER_LENGTH];
+	int length;
+};
+
+// Return the summation of two big integers
+struct big_integer sum_of_big_integer(struct big_integer a, struct big_integer b)
+{
+	// To do
+	return a;
+}
+
+
 // Tail recursive of fibonacci function
-int fibonacci(int n, int a, int b)
+struct big_integer fibonacci(int n, struct big_integer a, struct big_integer b)
 {
 	if (n == 0)
 		return a;
 	else if (n == 1)
 		return b;
 	else if (n < 0)
-		return 0;
+	{
+		struct	big_integer zero;
+		zero.unit_integer[0]=0;
+		zero.length = 1;
+		return zero;
+	}
 	else
-		return fibonacci(n-1, b, a+b);
+		return fibonacci(n-1, b, sum_of_big_integer(a,b));
+}
+
+// Print the given big integer number
+void print_big_integer (struct big_integer I)
+{
+	int i;
+	for(i=I.length; i>0; i++)
+		printf("%d",I.unit_integer[i-1]);
+	printf("\n");
 }
 
 int main(void)
@@ -19,8 +49,14 @@ int main(void)
 	int n;
 	scanf("%d", &n);
 
+	// Initialize big_integer
+	struct big_integer a, b;
+	a.length = 1;
+	b.length = 1;
+	b.unit_integer[0]=1;
+
 	// Print the output of fibonacci(n)
-	printf("%d\n", fibonacci(n, 1, 1));
+	print_big_integer(fibonacci(n, a, b));
 
 	return 0;
 }
