6d40230efc9069e9f3fb973aa242c0471b96eaf8
yeongjinoh <oyj9109@snu.ac.kr>
2016-03-26 01:29:42 +0900

    fibonacci - tail recursive

--- fibonacci/fibonacci.c
+++ fibonacci/fibonacci.c
@@ -1,23 +1,26 @@
 #include <stdio.h>
 
-int fibonacci(int n)
+// Tail recursive of fibonacci function
+int fibonacci(int n, int a, int b)
 {
-	if (n == 0 || n == 1)
-		return 1;
+	if (n == 0)
+		return a;
+	else if (n == 1)
+		return b;
 	else if (n < 0)
 		return 0;
 	else
-		return fibonacci(n-1) + fibonacci(n-2);
+		return fibonacci(n-1, b, a+b);
 }
 
 int main(void)
 {
 	// Get input n
 	int n;
-	scanf("%d",&n);
+	scanf("%d", &n);
 
 	// Print the output of fibonacci(n)
-	printf("%d\n",fibonacci(n));
+	printf("%d\n", fibonacci(n, 1, 1));
 
 	return 0;
 }
