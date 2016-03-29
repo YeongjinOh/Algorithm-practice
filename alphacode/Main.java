2836711944eaf8e2bd2de676cc5771dc55eea213
yeongjinoh <oyj9109@snu.ac.kr>
2016-03-29 20:53:35 +0900

    alphacode - Add alphacode class

--- alphacode/Main.java
+++ alphacode/Main.java
@@ -1,14 +1,33 @@
 import java.util.Scanner;
 
+class alphacode {
+
+	public String str;
+
+	public alphacode(String str)
+	{
+		this.str = str;
+	}
+
+	public int length() {
+		return str.length();
+	}
+
+	public int get(int idx) {
+		return str.charAt(idx)-'0';
+	}
+};
+
+
 public class Main{
 	public static void main(String[] args) {
 		
 		Scanner in = new Scanner(System.in);
-		String str = in.next();
+		alphacode a = new alphacode(in.next());
 
-		System.out.println(str.charAt(0)-'0');
-
-
+		int l = a.length();
+	
+		System.out.println(a.get(0) + "length :" + l);
 
 	}
 }
