30e0a48b2746ad9d57b14edc8ce206551a90d4cf
yeongjinoh <oyj9109@snu.ac.kr>
2016-03-29 20:47:17 +0900

    alphacode - skeleton

---
+++ alphacode/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main{
+	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);
+		String str = in.next();
+
+		System.out.println(str.charAt(0)-'0');
+
+
+
+	}
+}
