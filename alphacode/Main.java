751a35c3aa080aad73b4ed6b70a2617fb9dfe2e6
yeongjinoh <oyj9109@snu.ac.kr>
2016-03-29 22:45:30 +0900

    alphacode - Complete (java version)

--- alphacode/Main.java
+++ alphacode/Main.java
@@ -16,6 +16,48 @@ class alphacode {
 	public int get(int idx) {
 		return str.charAt(idx)-'0';
 	}
+
+	public boolean checkLast (int idx) {
+		return idx == (str.length()-1);
+	}
+
+	public boolean checkTwoDigits (int idx) {
+		if (checkLast(idx)) {
+			return false;
+		} else if (get(idx) == 1) {
+				return true;
+		} else if (get(idx) == 2) {
+				return (get(idx+1) <= 6);
+		} else
+			return false;
+	}
+
+	public boolean checkOneDigit (int idx) {
+		return (get(idx) != 0);
+	}
+
+	// a is the number of cases that this idx is involved
+	// and b is the number of cases that this idx is NOT involved
+	public double numberofCases (int idx, double a, double b) {
+		double mod = 1000000;
+		if (checkLast(idx)) {
+			if(checkOneDigit(idx))
+				return (a+b)%mod;
+			else
+				return a%mod;
+		}	else {
+			if (!checkOneDigit(idx))
+				return numberofCases (idx+1, 0, a)%mod;
+			else if (checkTwoDigits(idx)) {
+				return numberofCases (idx+1, b, a+b)%mod;
+			}
+			else {
+				return numberofCases (idx+1, 0, a+b)%mod;
+
+
+			}
+		}
+	}
 };
 
 
@@ -24,10 +66,9 @@ public class Main{
 		
 		Scanner in = new Scanner(System.in);
 		alphacode a = new alphacode(in.next());
-
-		int l = a.length();
+		
+		System.out.print((int)(a.numberofCases(0, 0, 1)));
 	
-		System.out.println(a.get(0) + "length :" + l);
 
 	}
 }
