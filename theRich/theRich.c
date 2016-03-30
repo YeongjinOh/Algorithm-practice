c68fad9c2c85a524636b4f6ca0f4b72f37e90756
YeongjinOh <oyj9109@snu.ac.kr>
2016-03-30 21:31:34 +0900

    The rich - need to reduce time complexity

--- theRich/theRich.c
+++ theRich/theRich.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 // the maximal length of array
-#define al 10000
+#define al 10001
 
 int main(void)
 {
@@ -10,52 +10,57 @@ int main(void)
 	scanf("%d%d",&M,&N);
 
 	// get the value of each coin
-	int i;
+	int i, j;
 	int value[N];
 	for (i=0; i<N; i++)
 		scanf("%d",&value[i]);
-	
-	// calculate the number of ways to make the total amount M
+
+
+	/* calculate the number of ways to make the total amount M */
+
 	// initailzie
-	int num[M/al+1][al];
-	int j;
-	for (i=0; i<=M/al; i++)
-		for(j=0; j<al; j++)
-			num[i][j]=-1;
+	int num[al];
+	for(i=0; i<al; i++)
+			num[i]=0;
 	for (i=0; i<N; i++)
-		num[value[i]/al][value[i]%al] = 1;
+		num[value[i]] = 1;
 	
-	// calculate
+	// each step calculates the number of ways up to the total amout i
 	for (i=1; i<=M; i++)
 	{
-		int	min = -2;
+		int	min = 0;
+
 		for(j=0; j<N; j++)
 		{
+			// Ignore the coin with the value higher than total amout i
 			if( value[j] >= i)
 				continue;
 
-			int refRow, refCol;
-			if (i%al >= value[j]) {
-				refRow = i/al;
-				refCol = i%al - value[j];
-			}	else {
-				refRow = (i/al)-1;
-				refCol = (i-value[j])%al;
-			}
+			// Check the previous step to the i-value[j]
+			int prev;
+			if (i%al >= value[j]) 
+				prev = i%al - value[j];
+			else 
+				prev = (i-value[j])%al;
 			
-			int cur = num[refRow][refCol];
-			if (cur>0)
-			{
-				if (min<0 || min>cur)
+			// Compare current value with minimum value
+			int cur = num[prev];
+			if (cur>0 && (min == 0 || min > cur))
 					min = cur;
-			}
 		}
-		if (min>0 && num[i/al][i%al] < 0)
-			num[i/al][i%al] = min+1;
+		
+		// If it's on the first iteration, check if the coin which has the same value with current i exists
+		if (i <= al)
+		{
+			if (min > 0 && num[i%al] == 0)
+				num[i%al] = min+1;
+		} else {
+			if (min > 0)
+				num[i%al] = min+1;
+		}
 	}
 
-
-	printf("%d\n",num[M/al][M%al]);
+	printf("%d\n",num[M%al]);
 
 	return 0;
 }
