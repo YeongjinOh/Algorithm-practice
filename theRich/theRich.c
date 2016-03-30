b28d24acfe046ebb4eedb7a24f8cd54a3d1fb62f
YeongjinOh <oyj9109@snu.ac.kr>
2016-03-30 21:08:32 +0900

    The rich - memory issue of array for large input

--- theRich/theRich.c
+++ theRich/theRich.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 
+// the maximal length of array
+#define al 10000
+
 int main(void)
 {
 	// get initial input. M is the amount of money the rich owns, N is the number of kind of coins. 
@@ -10,10 +13,49 @@ int main(void)
 	int i;
 	int value[N];
 	for (i=0; i<N; i++)
-		scanf("%d",&value[N]);
+		scanf("%d",&value[i]);
+	
+	// calculate the number of ways to make the total amount M
+	// initailzie
+	int num[M/al+1][al];
+	int j;
+	for (i=0; i<=M/al; i++)
+		for(j=0; j<al; j++)
+			num[i][j]=-1;
+	for (i=0; i<N; i++)
+		num[value[i]/al][value[i]%al] = 1;
+	
+	// calculate
+	for (i=1; i<=M; i++)
+	{
+		int	min = -2;
+		for(j=0; j<N; j++)
+		{
+			if( value[j] >= i)
+				continue;
+
+			int refRow, refCol;
+			if (i%al >= value[j]) {
+				refRow = i/al;
+				refCol = i%al - value[j];
+			}	else {
+				refRow = (i/al)-1;
+				refCol = (i-value[j])%al;
+			}
+			
+			int cur = num[refRow][refCol];
+			if (cur>0)
+			{
+				if (min<0 || min>cur)
+					min = cur;
+			}
+		}
+		if (min>0 && num[i/al][i%al] < 0)
+			num[i/al][i%al] = min+1;
+	}
 
 
-	printf("M:%d, N:%d\n",M,N);
+	printf("%d\n",num[M/al][M%al]);
 
 	return 0;
 }
