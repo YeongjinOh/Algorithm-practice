64447a5dba7ce27c02eae60054de6e4c8f730f3e
yeongjinoh <oyj9109@snu.ac.kr>
2016-04-03 23:44:41 +0900

    biomagnification - add getSum function

--- 4.dynamic_programming/biomagnification/biomagnification.c
+++ 4.dynamic_programming/biomagnification/biomagnification.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 int N;
+int pt=0;
 
 int getSum (int C[], int H[], int E[], int sum[], int prey_list[N][N], int prey_num[], int idx);
 
@@ -32,25 +33,12 @@ int main(void)
 		prey_list[life-1][prey_num[life-1]++] = prey-1;
 	}
 
-	/*
-	int j, min;
-	for (i=0; i<N; i++)
-	{
-		printf("The list of prey of %d : ",i+1);
-		min = prey_num[i];
-		for(j=0; j<min; j++)
-			printf("%d ",prey_list[i][j]+1);
-		printf("\n");
-	}
-	*/
-
 	int human_heavy = getSum(C,H,E,sum,prey_list,prey_num,N-1);
 	if (H[N-1] > human_heavy)
 		printf("yes\n%d\n",human_heavy);
 	else
-		printf("no");
+		printf("no\n");
 
-	
 	return 0;
 }
 
@@ -59,18 +47,18 @@ int getSum (int C[], int H[], int E[], int sum[], int prey_list[N][N], int prey_
 {
 	if (sum[idx])
 		return sum[idx];
-	int i, j;
+	int i, j, prey;
 	for (i=0; i<prey_num[idx]; i++)
 	{
 		// index of current prey
-		int prey = prey_list[idx][i];
+		prey = prey_list[idx][i];
 
 		if (sum[prey] == 0)
 			sum[prey] = getSum (C, H, E, sum, prey_list, prey_num, prey);
-		if (sum[prey] >= H[prey])
+		if (C[prey] != 0 && sum[prey] >= H[prey])
 		{
 			// if it dies, reduce prey list
-			for(j=i; j++; j<--prey_num[idx])
+			for(j=i; j<--prey_num[idx]; j++)
 				prey_list[idx][j] = prey_list[idx][j+1];
 			i--;
 		}
@@ -80,12 +68,61 @@ int getSum (int C[], int H[], int E[], int sum[], int prey_list[N][N], int prey_
 	if (prey_num[idx] == 0)
 		return H[idx];
 	
+	// calculate maximal energy for this inequation (a1E1 + ... + anEn) >= C
+	int max_E=0, max_idx;
 	for (i=0; i<prey_num[idx]; i++)
 	{
+		prey = prey_list[idx][i];
+		if (max_E < E[prey])
+		{
+			max_E = E[prey];
+			max_idx = prey;
+		}
+	
+
+		if(pt && idx == N-1)
+			printf("i:%d, max_E:%d\n",i,max_E);
+	}
+
+	int sumE, sumH[C[idx]+max_E], prev, min_sumH=0;
+	for (i=0; i<C[idx]+max_E; i++)
+		sumH[i]=0;
+
+	for (sumE=1; sumE < C[idx]+max_E; sumE++)
+	{
+		for (i=0; i<prey_num[idx]; i++)
+		{
+			prey = prey_list[idx][i];
+			
+			if (sumE < E[prey])
+				continue;
+			else if (sumE == E[prey])
+			{
+				if(sumH[sumE] == 0)	{
+					sumH[sumE] = sum[prey];
+				} else if(sumH[sumE]>sum[prey]) {
+					sumH[sumE] = sum[prey];
+				}
+				continue;
+			}
+
+			prev = sumH[sumE-E[prey]];
+			if (prev == 0)
+				continue;
+			if (sumH[sumE] == 0 || sumH[sumE] > prev+sum[prey])
+				sumH[sumE]=prev+sum[prey];
+		}
 		
 
-
+	// find minimal sum of H which is min(a1S1 + ... + anSn)
+		if (sumE == C[idx])
+			min_sumH = sumH[sumE];
+		else if (sumE > C[idx] && ((min_sumH == 0) || (min_sumH > sumH[sumE] && sumH[sumE])) != 0)
+		min_sumH = sumH[sumE];
 	}
+		if(pt)
+			printf("idx:%d, min_sumH:%d\n",idx,min_sumH);
+		return min_sumH;
 }
 		
 
