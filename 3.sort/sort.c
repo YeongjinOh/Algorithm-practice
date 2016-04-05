597a1da96227ce3773be64fff0f6e0c2206efc54
yeongjinoh <oyj9109@snu.ac.kr>
2016-04-06 03:16:46 +0900

    sort - add insertion sort and print the running time of each sorting to compare them

--- 3.sort/sort.c
+++ 3.sort/sort.c
@@ -3,13 +3,35 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-#define LENGTH 30
-#define MOD 100
+#define LENGTH 10000
+#define MOD 100000
 
-void bubble_sort(int val[], int length)
+// Default : descending order
+int back_up[LENGTH];
+
+void sort(void (*sort_func)(int *), char sort_name[]);
+
+void set_array(int rand_num[])
+{
+	int i;
+	for (i=0; i<LENGTH; i++)
+		rand_num[i] = back_up[i];
+
+}
+
+bool check(int val[])
+{
+	int i;
+	for (i=0; i<LENGTH-1; i++)
+		if (val[i] < val[i+1])
+			return false;
+	return true;
+}
+
+void bubble_sort(int val[])
 {
 	int i, j, tmp;
-	for (i=length-1; i>0; i--)
+	for (i=LENGTH-1; i>0; i--)
 	{
 		bool swapped = false;
 		for (j=0; j<i; j++)
@@ -25,25 +47,58 @@ void bubble_sort(int val[], int length)
 	}
 }
 
+void insertion_sort(int val[])
+{
+	int i, j, tmp;
+	for (i=1; i<LENGTH; i++)
+		for (j=i; j>0 && val[j-1]<val[j]; j--)
+		{
+			tmp = val[j];
+			val[j] = val[j-1];
+			val[j-1] = tmp;
+		}
+}
+
 
 int main(void)
 {
 	srand(time(NULL));
 	
 	// generate random number
-	int rand_num[LENGTH], i;
+	int i;
 	for (i=0; i<LENGTH; i++) {
-		rand_num[i]= rand()%MOD;
+		back_up[i]= rand()%MOD;
 	}
-
-	// sort the random number array
-	bubble_sort(rand_num,LENGTH);
 	
-	// print sorted array
-	for (i=0; i<LENGTH; i++) {
-		printf("%d ",rand_num[i]);
-	}
-	printf("\n");
+	printf("The LENGTH of random elements : %d\n", LENGTH);
 
+	sort(bubble_sort,"Bubble");
+	sort(insertion_sort, "Insertion");
+	
 	return 0;
+}
+
+void sort(void (*sort_func)(int *), char * sort_name) 
+{
+	// initialization
+	clock_t begin, end;
+	double time_spent;
+	int rand_num[LENGTH];
+	set_array(rand_num);
+	
+	// sort the random number array
+	begin = clock();
+	(*sort_func)(rand_num);
+	end = clock();
+	time_spent = (double)(end - begin)/CLOCKS_PER_SEC*1000;
+	if (check(rand_num))
+		printf("%s sort takes %.1f(ms)\n",sort_name, time_spent);
+	else
+	{
+		int i;
+		printf("%s sort fails\n",sort_name);
+		for (i=0; i<LENGTH; i++)
+			printf("%d ",rand_num[i]);
+		printf("\n");
+	}
 }
