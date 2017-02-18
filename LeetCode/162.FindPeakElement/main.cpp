bc134ff5bbb7c54a32386525e55d89a248ee93ad
yeongjinoh <oyj9109@snu.ac.kr>
2017-02-18 17:58:45 +0900

    LeetCode) Find Peak Element - fix error (one exception case)

--- LeetCode/162.FindPeakElement/main.cpp
+++ LeetCode/162.FindPeakElement/main.cpp
@@ -33,6 +33,8 @@ class Solution {
     public:
         int findPeakElement(vector<int>& nums) {
             int n = nums.size();
+            if (n==1)
+                return 0;
             if (nums[0] > nums[1])
                 return 0;
             if (nums[n-2] < nums[n-1])
