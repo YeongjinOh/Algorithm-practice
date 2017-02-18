36f80ecff7c1ba08f5db6d50fe05bfa4d4ecab4d
yeongjinoh <oyj9109@snu.ac.kr>
2017-02-19 01:15:19 +0900

    LeetCode) Combination Sum 3 - backtracking

--- LeetCode/216.CombinationSum3/main.cpp
+++ LeetCode/216.CombinationSum3/main.cpp
@@ -4,27 +4,26 @@ using namespace std;
 
 class Solution {
     public:
-        vector<vector<int> > helper(int end, int k, int n) {
-            vector<vector<int> > ret;
-            if (k == 1) {
-                if (end >= n && n >= 1) {
-                    vector<int> ps;
-                    ps.push_back(n);
-                    ret.push_back(ps);
-                }
-                return ret;
+        void backtrack(vector<vector<int> > & ret, vector<int> & nums, int k, int n) {
+            if (k==0) {
+                if (n==0)
+                    ret.push_back(nums);
+                return;
             }
-            for (int i=end; i>0; --i) {
-                vector<vector<int> > tmp = helper(i-1, k-1, n-i);
-                for (int j=0; j<tmp.size(); ++j) {
-                    tmp[j].push_back(i);
-                    ret.push_back(tmp[j]);
-                }
+            int start = 1;
+            if (nums.size() > 0)
+                start = nums[nums.size()-1]+1;
+            for (int i=start; i<=9; ++i) {
+                nums.push_back(i);
+                backtrack(ret, nums, k-1, n-i);
+                nums.pop_back();
             }
-            return ret;
         }
         vector<vector<int> > combinationSum3(int k, int n) {
-            return helper(9,k,n);
+            vector<vector<int> > ret;
+            vector<int> nums;
+            backtrack(ret, nums, k, n);
+            return ret;
         }
 };
 
@@ -32,7 +31,8 @@ int main(void) {
     Solution s;
     int a, b;
     cin >> a >> b;
-    vector<vector<int> > res = s.combinationSum3(a,b);
+   vector<vector<int> > res = s.combinationSum3(a,b);
+
     for (int i=0; i<res.size(); ++i) {
         for (int j=0; j<res[i].size(); ++j) {
             cout << res[i][j] << ", ";
