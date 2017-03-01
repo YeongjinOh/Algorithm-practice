99c31abece73e043e78647dc705445a3f4d5595b
yeongjinoh <oyj9109@snu.ac.kr>
2017-03-01 13:45:30 +0900

    algospot) Zimbabwe - DP O(n*m*2^n) where n = log(e)

--- algospot/Zimbabwe/main.cpp
+++ algospot/Zimbabwe/main.cpp
@@ -7,30 +7,36 @@
 using namespace std;
 int C, m, len;
 string e;
-bool visit[15];
 
-bool isMod(string price) {
-    long long sum = stoll(price);
-    return sum%m == 0;
-}
-int count (string price) {
+// 2 by 2^14 by m
+// i is bool which means price smaller than e // j = visit(1101010101) //  k = price[1:len] % m
+int cache[2][1024*16][20];
+
+// smaller means higher digits of price is already smaller than e,
+// so no need to compare
+// suppose that price[1:k] == e[1:k] if !smaller
+//              price[1:k] < e[1:k] if smaller
+int count (string price, int visit, int mod, bool smaller) {
+    int &ret = cache[smaller][visit][mod];
+    if (ret != -1) return ret;
+
     if (price.length() == len) {
-        if (price < e && isMod(price)) return 1;
+        if (smaller && mod == 0) return 1;
         return 0;
     }
-    int ret = 0;
+    ret = 0;
     bool digit[10];
     memset(digit,0,sizeof(digit));
     for (int i=0; i<len; ++i) {
-        if (digit[e[i]-'0'])
+        int d = e[i]-'0';
+        if (digit[d] || visit & (1<<i))
             continue;
-        bool& v = visit[i];
-        if (!v) {
-            v = true;
-            ret = (ret + count(price+e[i]))%MOD;
-            v = false;
-            digit[e[i]-'0'] = true;
-        }
+        if (!smaller && e[price.length()] < e[i])
+            continue;
+        int nextMod = ((mod*10)+d)%m;
+        ret += count(price+e[i], visit+(1<<i), nextMod%m, smaller || e[price.length()] > e[i]);
+        ret = ret % MOD;
+        digit[d] = true;
     }
     return ret;
 }
@@ -40,7 +46,7 @@ int main() {
     while (C--) {
         cin >> e >> m;
         len = e.length();
-        memset(visit,0,sizeof(visit));
-        cout << count("") <<endl;
+        memset(cache,-1,sizeof(cache));
+        cout << count("",0,0, false) <<endl;
     }
 }
