9bef875db2e950e8f43b0e9620cd88ec89671405
yeongjinoh <oyj9109@snu.ac.kr>
2017-03-03 00:34:48 +0900

    TSP - optimize using reverse swapping

--- algospot/TSP/TSP3.cpp
+++ algospot/TSP/TSP3.cpp
@@ -2,15 +2,16 @@
 #include <cstdio>
 #include <algorithm>
 #include <cstring>
+#include <vector>
 
 using namespace std;
-#define LEN 16
+#define LEN 17
 #define INF 987654321
 int N;
 double dist[LEN][LEN];
 double cache[1<<LEN][LEN];
 double best;
-int cntTopDown, cntBottomUp;
+int cntTopDown, cntBottomUp, cntOptimized;
 
 double bottomUp(int cur, int visit) {
     cntBottomUp++;
@@ -26,6 +27,11 @@ double bottomUp(int cur, int visit) {
     return res;
 }
 
+// topdown에서는 DP를 섞어 쓰기가 힘들다!
+// 현재 state(cur, visit)에서의 curLength가 실제 지나온 경로 (같은 visit set 내에서) 다를 수 있으므로
+// 예를 들어, 같은 cur, visit에서 curLength가 10일 때 그 값을 DP에 저장해놓고, curLength가 5일 때 그대로 값을 쓰면 문제가 생긴다.
+// 웬만하면 섞어쓰지 말자!!!
+
 double topDown(int cur, int visit, double curLength) {
     cntTopDown++;
     if (visit == (2<<N)-1) {
@@ -36,11 +42,47 @@ double topDown(int cur, int visit, double curLength) {
     for (int i=1; i<=N; ++i) {
         double nextLength = curLength + dist[cur][i];
         if (visit & (1<<i)) continue;
+        // 1. 가지치기(pruning)
         if (nextLength > best) continue;
         res = min<double>(res, topDown(i, visit+(1<<i), nextLength));
     }
     return res;
 }
+
+// current p-a- ... -b-q
+// but p-b- ... -a-q is smaller or equal to.
+// then no more search
+bool checkMoreSearch (vector<int> & path) {
+    if (path.size() < 5) return true;
+    int p = path[1], a = path[2];
+    for (int i=3; i<path.size()-1; i++) {
+        int b = path[i], q = path[i+1];
+        if (dist[p][a] + dist[b][q] >= dist[p][b] + dist[a][q])
+            return false;
+    }
+    return true;
+}
+
+double optimizedTopDown(vector<int> & path, int visit, double curLength) {
+    cntOptimized++;
+    if (visit == (2<<N)-1) {
+        best = min<double>(best,curLength);
+        return curLength;
+    }
+    if (!checkMoreSearch(path)) return INF;
+    int cur = path.back();
+    double res = INF;
+    for (int i=1; i<=N; ++i) {
+        if (visit & 1<<i) continue;
+        if (best < curLength + dist[cur][i]) continue;
+        path.push_back(i);
+        res = min<double>(res, optimizedTopDown(path, visit + (1<<i), dist[cur][i] + curLength));
+        path.pop_back();
+    }
+    return res;
+}
+
+
 int main() {
     int C;
     cin >> C;
@@ -52,11 +94,15 @@ int main() {
                 dist[0][j] = 0;
             }
         memset(cache,0,sizeof(cache));
-        best = INF;
         cntTopDown = 0;
         cntBottomUp = 0;
-        printf("topDown:%.10lf with cnt:%d\n",topDown(0,1,0.0), cntTopDown);
-        printf("bottomUp:%.10lf with cnt:%d\n",bottomUp(0,1), cntBottomUp);
+        vector<int> path;
+        path.push_back(0);
+        best = INF;
+        //printf("topDown:%.10lf \twith cnt:%d\n",topDown(0,1,0.0), cntTopDown);
+        printf("bottomUp:%.10lf \twith cnt:%d\n",bottomUp(0,1), cntBottomUp);
+        best = INF;
+        printf("Opt-Topdown:%.10lf \twith cnt:%d\n",optimizedTopDown(path,1,0.0), cntOptimized);
     }
     return 0;
 }
