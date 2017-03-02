1209cc2f25052f7ab16e9159870779e4b9251418
yeongjinoh <oyj9109@snu.ac.kr>
2017-03-03 01:48:08 +0900

    TSP - optimize by using minimal distance

--- algospot/TSP/TSP3.cpp
+++ algospot/TSP/TSP3.cpp
@@ -8,10 +8,11 @@ using namespace std;
 #define LEN 17
 #define INF 987654321
 int N;
-double dist[LEN][LEN];
+double dist[LEN][LEN], minDist[LEN];
 double cache[1<<LEN][LEN];
 double best;
 int cntTopDown, cntBottomUp, cntOptimized;
+vector<int> neighbors[LEN];
 
 double bottomUp(int cur, int visit) {
     cntBottomUp++;
@@ -63,46 +64,89 @@ bool checkMoreSearch (vector<int> & path) {
     return true;
 }
 
-double optimizedTopDown(vector<int> & path, int visit, double curLength) {
+bool checkAllVisit(vector<bool> & visit) {
+    for (int i=0; i<visit.size(); ++i)
+        if(!visit[i]) return false;
+    return true;
+}
+double optimizedTopDown(vector<int> & path, vector<bool> & visit, double curLength) {
     cntOptimized++;
-    if (visit == (2<<N)-1) {
+    if (checkAllVisit(visit)) {
         best = min<double>(best,curLength);
         return curLength;
     }
     if (!checkMoreSearch(path)) return INF;
     int cur = path.back();
     double res = INF;
+
+    // check sum of min distance
+    double minSum = 0.0;
+    if (visit.size() != N+1)
+        cout << "asdsad" << endl;
     for (int i=1; i<=N; ++i) {
-        if (visit & 1<<i) continue;
-        if (best < curLength + dist[cur][i]) continue;
-        path.push_back(i);
-        res = min<double>(res, optimizedTopDown(path, visit + (1<<i), dist[cur][i] + curLength));
+        if (!visit[i]) {
+            minSum += minDist[i];
+        }
+    }
+    if (curLength + minSum > best)
+        return INF;
+
+
+    for (int i=0; i<neighbors[cur].size(); ++i) {
+        int next = neighbors[cur][i];
+        if (visit[next]) continue;
+        if (best < curLength + dist[cur][next]) continue;
+        path.push_back(next);
+        visit[next] = true;
+        res = min<double>(res, optimizedTopDown(path, visit, dist[cur][next] + curLength));
+        visit[next] = false;
         path.pop_back();
     }
     return res;
 }
 
-
 int main() {
     int C;
     cin >> C;
+    memset(dist,0,sizeof(dist));
     while(C--){
         cin >> N;
-        for (int i=1; i<=N; ++i)
+        for (int i=1; i<=N; ++i) {
+            // initialize
+            double minD = INF;
+            vector<pair<double, int> > order;
+            neighbors[i].clear();
+            // get input
             for (int j=1; j<=N; ++j) {
                 cin >> dist[i][j];
-                dist[0][j] = 0;
+                if (i != j) {
+                    minD = min<double>(minD, dist[i][j]);
+                    order.push_back(make_pair(dist[i][j],j));
+                }
             }
-        memset(cache,0,sizeof(cache));
+            // pre process
+            sort(order.begin(),order.end());
+            for (int j=0; j<order.size(); ++j)
+                neighbors[i].push_back(order[j].second);
+            minDist[i] = minD;
+        }
+        neighbors[0].clear();
+        for (int i=1; i<=N; ++i)
+            neighbors[0].push_back(i);
+
+//        memset(cache,0,sizeof(cache));
         cntTopDown = 0;
         cntBottomUp = 0;
         vector<int> path;
+        vector<bool> visit (N+1, false);
+        visit[0] = true;
         path.push_back(0);
         best = INF;
         //printf("topDown:%.10lf \twith cnt:%d\n",topDown(0,1,0.0), cntTopDown);
-        printf("bottomUp:%.10lf \twith cnt:%d\n",bottomUp(0,1), cntBottomUp);
+//        printf("bottomUp:%.10lf \twith cnt:%d\n",bottomUp(0,1), cntBottomUp);
         best = INF;
-        printf("Opt-Topdown:%.10lf \twith cnt:%d\n",optimizedTopDown(path,1,0.0), cntOptimized);
+//        printf("Opt-Topdown:%.10lf \twith cnt:%d\n",optimizedTopDown(path,visit,0.0), cntOptimized);
+        printf("%.10lf\n",optimizedTopDown(path,visit,0.0));
     }
     return 0;
 }
