1ccb834a0c1f9324fb534a27f446f2447b21444b
yeongjinoh <oyj9109@snu.ac.kr>
2017-03-02 14:07:50 +0900

    algospot) StrJoint - greedy using priority queue

--- algospot/StrJoin/main.cpp
+++ algospot/StrJoin/main.cpp
@@ -3,33 +3,33 @@
 #include <vector>
 #include <algorithm>
 #include <set>
+#include <queue>
 #define LEN 100
 
 using namespace std;
 int C, n, len;
-multiset<int> s;
+//multiset<int> s;
+priority_queue<int, vector<int>, greater<int> > pq;
 
 int main() {
     cin >> C;
     while (C--) {
-        s.clear();
+//        s.clear();
         cin >> n;
         for (int i=0; i<n; i++) {
             cin >> len;
-            s.insert(len);
+            // s.insert(len);
+            pq.push(len);
         }
         int sum = 0, cur;
         multiset<int>::iterator it;
-        while (s.size() > 1) {
-            it = s.begin();
-            cur = *it;
-            s.erase(it);
-            it = s.begin();
-            cur += *it;
-            s.erase(it);
+        while (pq.size() > 1) {
+            cur = pq.top(); pq.pop();
+            cur += pq.top(); pq.pop();
             sum += cur;
-            s.insert(cur);
+            pq.push(cur);
         }
         cout << sum << endl;
+        pq.pop();
     }
 }
