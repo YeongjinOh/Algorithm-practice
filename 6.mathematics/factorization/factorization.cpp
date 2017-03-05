ecfa9e0dac62846afcaf25cc4b2dea9c1c5938e8
yeongjinoh <oyj9109@snu.ac.kr>
2017-03-06 01:18:38 +0900

    Baekjoon) factorization - optimize using Sieve of eratosthenes 12ms

--- 6.mathematics/factorization/factorization.cpp
+++ 6.mathematics/factorization/factorization.cpp
@@ -1,15 +1,34 @@
 #include <iostream>
 #include <algorithm>
+#include <cmath>
+#include <cstring>
+
 using namespace std;
+#define MAX 10000010
+
+// -1 means prime
+// 0 or positive means the smallest prime factor
+int isPrime[MAX];
+
 int main () {
     int n;
     cin >> n;
-    int start = 2;
-    for (int i = start; n>1; i++) {
-        while (n%i == 0) {
-            cout << i << endl;
-            n /= i;
+
+    memset(isPrime,-1,sizeof(isPrime));
+
+    // sieve of eratoshenes
+    for (int i=2; i*i<=n; i++) {
+        if (isPrime[i] == -1) {
+            for (int j=i*i; j<=n; j+=i)
+                if (isPrime[j] == -1)
+                    isPrime[j] = i;
         }
     }
+    while (isPrime[n] != -1) {
+        cout << isPrime[n] << endl;
+        n /= isPrime[n];
+    }
+    cout << n << endl;
+
     return 0;
 }
